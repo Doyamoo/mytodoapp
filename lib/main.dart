24b639e17078cf881b701a6d021c8b6b63510455
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-05-20 22:21:49 +0900

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //アプリ名
+      title: 'My Todo App',
+      theme: ThemeData(
+        //テーマカラー
+        primarySwatch: Colors.blue,
+      ),
+
+      home: TodoListPage(),
+    );
+  }
+}
+
+// リスト一覧画面用Widget
+class TodoListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('リスト追加画面　(クリックで戻る)')),
+      ),
+    );
+  }
+}
